03abd2654b9bcbc4dd0ff986502c6692052f3e12
Sxmon17 <simonguglberger@gmail.com>
2022-11-10 12:07:17 +0100

    added indented output

--- src/main/java/jsuc/jsuc.java
+++ src/main/java/jsuc/jsuc.java
@@ -35,20 +35,24 @@ public class jsuc implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         Subnet subnet = new Subnet(new IpAddress(ipAddress), new IpAddress(subnetmask));
+        String format = "%-30s%s%n";
         String output =
-                  "Suffix: " + subnet.getSuffix() + "\n"
-                + "Netaddress: " + subnet.getNetAddress() + "\n"
-                + "Broadcastaddress: " + subnet.getBroadcastAddress() + "\n"
-                + "Number of Hosts: " + subnet.getNumberOfHosts() + "\n"
-                + "First Ip in Network: " + subnet.getFirstIp() + "\n"
-                + "Last Ip in Network: " + subnet.getLastIp() + "\n"
-                + "Next Subnet: " + subnet.getNextSubnet();
-        System.out.println(output);
-        colorOutput(output);
+                  String.format(format, "Suffix: ", subnet.getSuffix())
+                + String.format(format, "Netaddress: ", subnet.getNetAddress())
+                + String.format(format, "Broadcastaddress: ", subnet.getBroadcastAddress())
+                + String.format(format, "Number of Hosts: ", subnet.getNumberOfHosts())
+                + String.format(format, "First Ip in Network: ", subnet.getFirstIp())
+                + String.format(format, "Last Ip in Network: ", subnet.getLastIp())
+                + String.format(format, "Next Subnet: ", subnet.getNextSubnet());
+
+        System.out.println(colorOutput(output));
         if(save) {
             try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("/home/simon/IdeaProjects/jsuc/src/main/resources/content.txt"));
-                writer.write(output);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(
+                        "/home/simon/IdeaProjects/jsuc/src/main/resources/content.txt"
+                ));
+                //String.format(format, "Next Subnet: ", subnet.getNextSubnet()).length();
+                writer.write("-----------" + subnet + "-----------" + "\n" + output);
                 writer.close();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -63,14 +67,12 @@ public class jsuc implements Callable<Integer> {
 
         int index1 = sb.indexOf(":");
         while(index1 >= 0) {
-            System.out.println(index1);
             sb.insert(index1+1, GREEN);
             index1 = sb.indexOf(":", index1+1+GREEN.length());
         }
 
         int index2  = sb.indexOf("\n");
         while(index2 >= 0) {
-            System.out.println(index2);
             sb.insert(index2, RESET);
             index2 = sb.indexOf("\n", index2+1+RESET.length());
         }
